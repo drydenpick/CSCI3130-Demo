614f6c8a85283ee540bcf0b28537ce5a56186ea6
Dryden Pick <dr286855@dal.ca>
2018-05-18 11:49:09 -0300

    Completing assignment 1

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button_id);
+        final EditText input = findViewById(R.id.editText_id);
+        final TextView output = findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //The assignment merely said copied to the TextView. I could easily append it or something.
+                output.setText(input.getText());
+            }
+        });
     }
 }
